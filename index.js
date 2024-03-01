2be4ad0c69be8e09221657f5a490cc820997568a
Zia ul Qamar <140687816+ziaulqamardev@users.noreply.github.com>
2024-03-02 02:17:41 +0500

    Code API

---
+++ index.js
@@ -0,0 +1,40 @@
+// index.js
+
+// Import required modules
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/api/prodcut", async (req, res) => {
+  try {
+    const response = await axios.get("https://dummyjson.com/products");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// POST endpoint
+app.post("/api/createProduct", async (req, res) => {
+  try {
+    const { productName, price } = req.body;
+
+    // Check if productName and price are present in the request body
+    if (!productName || !price) {
+      throw new Error("productName and price are required");
+    }
+
+    console.log(`Creating product: ${productName} with price: ${price}`);
+
+    // Send a success response
+    res.status(201).json({ message: "Product created successfully" });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
