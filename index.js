ca61b360ef5891056fb3ba8705ad27c0d23ecd00
Zia ul Qamar <140687816+ziaulqamardev@users.noreply.github.com>
2024-04-02 14:51:15 +0500

    improve html

--- index.js
+++ index.js
@@ -41,24 +41,32 @@ app.post("/api/createProduct", async (req, res) => {
 const jsonResponse = [
   { id: "S00001099", content: "Here is the content for shipment S00001099" },
   { id: "S00001100", content: "Here is the content for shipment S00001100" },
-  { id: "S00001101", content: "Here is the content for shipment S00001101" }
+  { id: "S00001101", content: "Here is the content for shipment S00001101" },
 ];
 
 // Express route
-app.get('/api/html', (req, res) => {
+app.get("/api/html", (req, res) => {
   const htmlResponse = `
     <ul>
+    ${jsonResponse
+      .map(
+        (shipment, index) =>
+          `
       <li>
-        ${jsonResponse.map((shipment, index) => (
-          `<div class="rounded border border-blue-500 p-2 my-2" key=${index}>
-            <div class="flex w-full justify-between" onClick="toggleDetails(${index}, '${shipment.id}')">
-              <p>SHIPMENT ID: ${shipment.id}</p>
-              <span class="text-lg cursor-pointer" id="toggleIcon${index}">⯅</span>
-            </div>
-            <p id="details${index}" style="display:none;">${shipment.content}</p>
-          </div>`
-        )).join('')}
-      </li>
+          <div className="rounded border border-blue-500 p-2 my-2" key="${index}">
+          <div className="flex w-full justify-between" onClick={() => toggleDetails(${index}, ${shipment.id})}>
+            <p>${shipment.id}</p>
+            <span className="text-lg cursor-pointer">{${openIndex} === ${index} ? "⯆" : "⯅"}</span>
+          </div>
+          {${openIndex} === ${index} && <p>Rest of the details...</p>}
+        </div>
+
+
+
+          </li>
+          `
+      )
+      .join("")}
     </ul>
   `;
 
