03eaff206a2acc7827284a7fe3790d7ce0b16759
nomanaslam.it@gmail.com <nomanaslam.it@gmail.com>
2024-04-04 13:54:39 +0500

    revrce change 7

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.get("/api/html", (req, res) => {
         <div class="rounded border border-blue-500 p-2 my-2">
           <div class="flex w-full justify-between" data-shipment-id="${shipment.id}">
             <p id="shipment-id">${shipment.id}</p>
-            <span class="text-lg cursor-pointer toggle-button" onclick="toggleDetails(${shipment.id})">+</span>
+            <span class="text-lg cursor-pointer toggle-button">+</span>
           </div>
           <p class="details hidden">${shipment.content}</p>
         </div>
@@ -61,7 +61,7 @@ app.get("/api/html", (req, res) => {
   });
 
   renderedHtml += `</ul>`;
-
+  
   res.send(renderedHtml);
 });
 
